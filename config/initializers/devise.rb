a32d15b36c795ebcc2707c7e2b9992b7381dc9fd
ikun <isao.miyagawa.2019@gmail.com>
2021-03-09 01:05:12 +0900

    認証メールの文面を変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,8 +27,7 @@ Devise.setup do |config|
   config.mailer_sender = ENV['SMTP_USER_NAME']
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
-
+  config.mailer = 'Devise::Mailer'
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
 
