7ac0416a77afd5030cfbb2ff6377ce48489b8252
ikun <isao.miyagawa.2019@gmail.com>
2021-03-09 22:44:37 +0900

    アソシエーション完了

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -9,7 +9,7 @@ class Engineer < ApplicationRecord
   has_many :requests, through: :orders
   has_many :agreements
   has_many :completes
-  has_many :payment_histories
+  has_many :cards, dependent: :destroy
 
   def name
     self.first_name + " " + self.last_name
