81844475b13ad791ae15d10f9e84073950d741a2
ikun <isao.miyagawa.2019@gmail.com>
2021-03-16 22:49:53 +0900

    topページのデザイン変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require("../bootstrap.bundle")
 require("../jquery-3.5.1")
 require("../lazysizes.min.js")
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
