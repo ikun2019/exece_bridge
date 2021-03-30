72044d05caea26a1ae6d1e743e80689839cc9b63
ikun <isao.miyagawa.2019@gmail.com>
2021-03-30 22:43:41 +0900

    cssの調整完了

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module ExeceBridge
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.assets.paths << config.root.join("vendor/assets/javascripts")
+    config.assets.paths << config.root.join("vendor/assets/stylesheets")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
