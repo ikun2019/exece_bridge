7706adf5e65d60bfee588b390c26eab8f3d37817
ikun <isao.miyagawa.2019@gmail.com>
2021-04-06 00:54:20 +0900

    deviseのremember_meの調整

--- app/controllers/customer/parameter_sanitizer.rb
+++ app/controllers/customer/parameter_sanitizer.rb
@@ -3,5 +3,6 @@ class Customer::ParameterSanitizer < Devise::ParameterSanitizer
     super
     permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :image, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number])
     permit(:account_update, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :image, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number])
+    permit(:sign_in, keys: [:remember_me])
   end
 end
--- app/controllers/engineer/parameter_sanitizer.rb
+++ app/controllers/engineer/parameter_sanitizer.rb
@@ -3,5 +3,6 @@ class Engineer::ParameterSanitizer < Devise::ParameterSanitizer
     super
     permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :image])
     permit(:account_update, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :study_id, :language_id, :profile, :image])
+    permit(:sign_in, keys: [:remember_me])
   end
 end
